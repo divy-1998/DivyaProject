97199e218fb984389913940290cb110b2fdd356a
PAMULA DIVYA RAJESWARI <divyarajeswaripamula@gmail.com>
2022-11-26 17:14:24 +0000

    change Math.java

--- Math.java
+++ Math.java
@@ -8,6 +8,7 @@ public class Math {
     public static int div(int x,int y){
         return x/y;
     }
+    public static int mul(int x,int y){return x*y;}
     public static void main(String[] args){
        System.out.println(div(20,2));
     }
