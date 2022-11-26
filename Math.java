a3cb77f043d196f76b06269f576bc2c96bf0d389
PAMULA DIVYA RAJESWARI <divyarajeswaripamula@gmail.com>
2022-11-26 17:03:27 +0000

    added Math.java

--- Math.java
+++ Math.java
@@ -1,2 +1,14 @@
 public class Math {
+    /********************************
+     Tester: DIVYA RAJESWARI PAMULA
+     University ID: 220318055
+     Function: This function takes two integer as input, divide the first number by second number and returns the result.
+     **********************************/
+
+    public static int div(int x,int y){
+        return x/y;
+    }
+    public static void main(String[] args){
+       System.out.println(div(20,2));
+    }
 }
